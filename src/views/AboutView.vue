7a4f6c07128545795eb2c51b9a9c5a6ac7515f58
su-mao-xiong <sumaoxiong3963@gmail.com>
2025-04-17 16:22:01 +0800

    hash history

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,6 +4,25 @@
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      data: {},
+    }
+  },
+  mounted() {
+    console.log(import.meta.env.VITE_TEXT)
+    const url = import.meta.env.VITE_PATH
+    this.$http.get('https://randomuser.me/api/').then((res) => {
+      console.log(res)
+      this.data = res.data.results[0]
+      console.log(this.data)
+    })
+  },
+}
+</script>
+
 <style>
 @media (min-width: 1024px) {
   .about {
